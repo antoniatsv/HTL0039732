2116b6cacc721d8b2977eae6c4ecfe71678ec8da
Antonia Tsvetanova <antonia.tsvetanova@mrc-bsu.cam.ac.uk>
2024-02-21 14:00:05 +0000

    model updated

--- BLRM-code-12.02-Antonia.R
+++ BLRM-code-12.02-Antonia.R
@@ -13,120 +13,100 @@ doses <- c(20,40,80,160,320,640,960)
 n.patients.per.dose <- c(0,0,2,2,0,0,0)
 #total number of patients across all dose levels
 N <- sum(n.patients.per.dose) 
-#number of DLTs for each patient/dose level
-s <- c(0,0,0,0,0)
+#whether a patient had a DLT or not 
+s <- c(0,0,0,0)
 #current dose for each patient
 d <- c(80, 80, 160, 160)
 #reference dose
 D.ref <- 960
 #binary indicator I (0=montherapy, 1 = combination therapy)
-I.combo <- c(0,0,0,0)
+#I.combo <- c(0,0,0,0)
+#I.combo <- c(rep(0, length(d)), rep(1, length(d)))
 
 #vector of prior means 
 mu0 <- -1.75
 mu1 <- 0.00
 mu2 <- 0.00
-prior.mean <- c(mu0, mu1, mu2)
+priorMean <- c(mu0, mu1, mu2)
 
 #covariance matrix
-sigma0 <- 1.60^2
-sigma1 <- 0.2^2
-sigma2 <- 1.5^2
-corr <- 0
-cov.matrix <- matrix(c(sigma0, sigma0*sigma1*corr, sigma0*sigma2*corr,
-                       sigma0*sigma1*corr, sigma1, sigma1*sigma2*corr,
-                       sigma0*sigma2*corr, sigma1*sigma2*corr, sigma2), ncol = 3, byrow = TRUE)
-priorVar<-cov.matrix
+sigma0 <- 1.60
+sigma1 <- 0.2
+sigma2 <- 1.5
+priorVar <- matrix(c(sigma0^2, 0, 0,
+                     0, sigma1^2, 0,
+                     0, 0, sigma2^2), ncol = 3, byrow = TRUE)
+
+priorPrec <- solve(priorVar)
+
 #iterations
 iter<-4*10^6
 #overdose probability threshold
 c.overdose<-0.40 
-#number of DLTs on the current dose levels
-no.DLT = 0
+#number of DLTs on the current dose levels per each patient - no.DLT and s are essentially the same, no? 
+no.DLT = c(0,0,0,0,0)
 
 ############################################################################################################
 
 blrm.data <- list(
   D.ref = D.ref,
+  s = s,
   d = d,
-  I.combo = I.combo,
   N = N,
-  mu0 = mu0,
-  mu1 = mu1,
-  mu2 = mu2,
-  sigma0 = sigma0,
-  sigma1 = sigma1,
-  sigma2 = sigma2,
-  priorVar = priorVar
+  priorPrec = priorPrec,
+  priorMean = priorMean
 )
 
 
 
-############################################################################################################
+# Model specification for JAGS
 blrm.model.string <- "
 model {
   ## Priors ##
-  alpha0 ~ dnorm(mu0, 1/sigma0^2)
-  alpha1 ~ dnorm(mu1, 1/sigma1^2)
-  log.alpha2 ~ dnorm(log(mu2), 1/sigma2^2)
-  alpha2 <- exp(log.alpha2)
+  theta[1:3] ~ dmnorm(priorMean[], priorPrec[1:3,1:3])
 
-  Omega[1:3, 1:3] <- inverse(priorVar)
+  
+  alpha0 <- theta[1]
+  alpha1 <- exp(theta[2])
+  alpha2 <- exp(theta[3])
 
   ## Likelihood ##
   for (j in 1:N) {
-    lin.pred.p.1[j] <- alpha0 + alpha1 * log(d[j]/D.ref) + alpha2 * I.combo[j]
-    lin.pred.p.2[j] <- ifelse(lin.pred.p.1[j] < -10, -10, ifelse(lin.pred.p.1[j] > 10, 10, lin.pred.p.1[j])) # lin pred after applying constraints to prevent extreme values to make sure the values for p1 are within a certain range
-    s[j] ~ dbern(p[j])
+    lin.pred.p.1[j] <- alpha0 + alpha1 * log(d[j]/D.ref)
+    lin.pred.p.2[j] <- ifelse(lin.pred.p.1[j] < -10, -10, ifelse(lin.pred.p.1[j] > 10, 10, lin.pred.p.1[j]))
     p[j] <- exp(lin.pred.p.2[j]) / (1 + exp(lin.pred.p.2[j])) 
+    s[j] ~ dbern(p[j])
+    
   }
 }
 "
 
-#create a text connection object from the string
-blrm.model.spec <- textConnection(blrm.model.string)
-#data to be used by the blrm model 
-blrm.data <- list(
-  D.ref = D.ref,
-  d = d,
-  I.combo = I.combo,
-  N = N,
-  mu0 = mu0,
-  mu1 = mu1,
-  mu2 = mu2,
-  sigma0 = sigma0,
-  sigma1 = sigma1,
-  sigma2 = sigma2,
-  priorVar = priorVar
-)
-#create a JAGs model object 
-jags <- jags.model(file = textConnection(blrm.model.string), data = blrm.data)
-#MCMC iterations 'burn-in' to allow the sampler to reach its stationary distribution before collecting samples for inference
-update(jags, iter)
-#sampling from the posterior distribution of the specified alpha parameters from the JAGS model
-tt <-jags.samples(jags,c('alpha0','alpha1','alpha2'),iter,progress.bar="none")
-#extracting samples of the alpha parameters from the 'tt' object. it selects the first chain and all iterations for that chain. 
-a01<-tt$alpha0[1,,]
-a11<-(tt$alpha1[1,,])
-a21<-(tt$alpha2[1,,])
+blrm.model <- jags.model(textConnection(blrm.model.string), data = blrm.data, n.chains = 1)
+update(blrm.model, iter)
+samples <- jags.samples(blrm.model, variable.names = c("alpha0", "alpha1", "alpha2"), n.iter = iter)
+
+# Extract posterior samples
+a01 <- samples$alpha0[1, ,]
+a11 <- samples$alpha1[1, , ]
+a21 <- samples$alpha2[1, , ]
+
 
 
 ########################################
 #create empty vectors to store estimates 
-LCI<-UCI<-Pi.MTD<-Pi.MTD.store<-Pi.above<-toxicity<-mat.or.vec(length(d),1)
-all.Pi <-mat.or.vec(iter,length(d))
-stop <-0
+LCI<-UCI<-Pi.MTD<-Pi.MTD.store<-Pi.above<-toxicity<-mat.or.vec(length(doses),1)
+all.Pi <-mat.or.vec(iter,length(doses))
+allstop <-0
 
-for (j in 1:length(d)) {
-  LP <- a01 + a11 * log(d[j]/D.ref) + a21 * I.combo[j] 
+for (j in 1:length(doses)) {
+  LP <- a01 + a11 * log(doses[j]/D.ref)
   #make sure LP stays between -10 and 10
   LP <- pmin(pmax(LP, -10), 10)
   #calculate the odds
   odds <- exp(LP)
   #calculate the predicted probabilities for specific j
   Pi <- odds / (1 + odds)
-  #Pi for all j's
-  all.Pi[, j] <- Pi
+  
   #calculate Pi for DLT for individual patient j at their current dose level
   Pi.MTD.store[j] <- Pi.MTD[j] <- mean(Pi < 0.30) - mean(Pi < 0.20)
   #probability of toxicity greater than 30%
@@ -142,15 +122,14 @@ for (j in 1:length(d)) {
 Pi.MTD[Pi.MTD == 0] <- 0.001
 Pi.MTD[Pi.above > c.overdose] <- 0
 
-#68
+#create empty vectors to store new variables
+Pi.MTD.mono <- Pi.MTD[1:length(doses)]
+Pi.MTD.mono.store <- Pi.MTD.store[1:length(doses)]
+Pi.above.mono <- Pi.above[1:length(doses)]
+toxicity.mono <- toxicity[1:length(doses)]
+n.mono <- n.patients.per.dose[1:length(doses)]
+s.mono <- s[1:length(doses)]
+LCI.mono <- LCI[1:length(doses)]
+UCI.mono <- UCI[1:length(doses)]
 
-Pi.MTD.mono <- Pi.MTD[1:length(d)]
-Pi.MTD.mono.store <- Pi.MTD.store[1:length(d)]
-Pi.above.mono <- Pi.above[1:length(d)]
-toxicity.mono <- toxicity[1:length(d)]
-n.mono <- n.patients.per.dose[1:length(d)]
-s.mono <- s[1:length(d)]
-LCI.mono <- LCI[1:length(d)]
-UCI.mono <- UCI[1:length(d)]
-
-#######
+###
