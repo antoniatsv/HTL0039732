7a18c74e2548b31ac3fa6b6d699f9140ba9acf64
Antonia Tsvetanova <antonia.tsvetanova@mrc-bsu.cam.ac.uk>
2024-03-05 11:28:06 +0000

    model dose update

--- BLRM-code-Antonia.R
+++ BLRM-code-Antonia.R
@@ -297,3 +297,6 @@ print(output.table.combo)
 # Write to CSV files
 write.csv(output.table.mono, "Monotherapy-SRC-Meeting-07.03-Results.csv", row.names = TRUE, na="")
 write.csv(output.table.combo, "Combination-SRC-Meeting-07.03.Results.csv", row.names = TRUE, na="")
+
+
+###
